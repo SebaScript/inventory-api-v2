3174acc378ee46e9dc4a43061fb8af2728ba5636
SebaScript <jgarcia199@soyudemedellin.edu.co>
2026-09-03 22:29:00 -0500

    ✨ feat: mount the groups routes under /v2

--- src/groups/groups.module.ts
+++ src/groups/groups.module.ts
@@ -3,11 +3,12 @@ import { TypeOrmModule } from '@nestjs/typeorm';
 import { Group } from '../entities/group.entity';
 import { Item } from '../entities/item.entity';
 import { GroupsController } from './groups.controller';
+import { GroupsV2Controller } from './groups.v2.controller';
 import { GroupsService } from './groups.service';
 
 @Module({
   imports: [TypeOrmModule.forFeature([Group, Item])],
-  controllers: [GroupsController],
+  controllers: [GroupsController, GroupsV2Controller],
   providers: [GroupsService],
 })
 export class GroupsModule {}
